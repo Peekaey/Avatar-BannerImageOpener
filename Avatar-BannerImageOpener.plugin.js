abd214cf3f56b501429c7a63115c91230d709b06
Dallas <dallasjt31@gmail.com>
2022-07-11 13:00:01 +1000

    Initial Commit

---
+++ Avatar-BannerImageOpener.plugin.js
@@ -0,0 +1,35 @@
+/**
+ * @name Avatar-BannerImageOpener
+ * @description Allows you the ability to click on a users avatar or banner on their profile page which will open an external browser page to the image
+ * @version 1.0
+ * @author Peekay
+ * @source https://github.com/Sidewinderr/Avatar-BannerImageOpener
+ */
+
+
+// Main event listener
+
+module.exports = class Main {
+    start() {
+        document.addEventListener("click", this.OnClickFunction);
+    }
+
+    OnClickFunction({target}) {
+
+        // If the user clicks on an avatar, will run this statement
+
+        if (target.classList.contains('avatar-3QF_VA') ) {
+            var profilepictureUrl = target.querySelector("img").src
+            var link = profilepictureUrl.replace('size=128', 'size=4096')
+            window.open(link)
+        } 
+
+        // If the user clicks on a banner, will run this statement
+
+        if ( target.classList.contains('banner-1YaD3N') ) {
+            var bannerUrl = target.style.backgroundImage
+            var [ _, bannerLink, _] = bannerUrl.split("\"")
+            window.open(bannerLink)
+    }
+}
+}
---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+## Avatar & Banner Image Opener
+#### This is a plugin that was written to be used alongside [BetterDiscord](https://betterdiscord.app)
+
+The plugin allows the user the ability to click on other user's profile pictures or profile banners while viewing their profile. This plugin will then automatically obtain the URL to the image and open the default browser with a bigger version of the image.
+
+<details>
+<summary> Installation </summary>
+This plugin can be installed simply by dragging and dropping the file into your BetteDiscord plugins folder
+</details>
